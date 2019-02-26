8a52295cdffb37bf4297f03206bd598d188bf51d
Herman Singh <kartstig@gmail.com>
2019-02-25 20:43:06 -0500

    This changed for some reason

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule EctoChangesetTools.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       deps: deps(),
       docs: docs(),
       package: package()
