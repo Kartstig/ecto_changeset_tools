4faa1182c3e4b34fdbc33056f1a0c6d475c3741f
Herman Singh <kartstig@gmail.com>
2019-02-25 21:38:41 -0500

    1.0 :party

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoChangesetTools.MixProject do
   def project do
     [
       app: :ecto_changeset_tools,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
